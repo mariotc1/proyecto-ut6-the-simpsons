78f83e57a390d4c2d0b4a6a4e00e30ec732af161
mariotc1 <mariotomecore@gmail.com>
2026-01-14 11:51:56 +0100

    primer commit

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <h1>hola mundo, soy pie de página</h1>
+        )
+    }
+}
+
+export default Footer;
---
+++ src/app/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./NavBar.jsx";
+class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <NavBar />
+            </header>
+        )
+    }
+}
+
+export default Header;
